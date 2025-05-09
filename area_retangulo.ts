946025b4f75f74e01177bd73f49f558d43937e0a
GustavoTeixeira0 <gustavoteixeiram@unipam.edu.br>
2025-05-09 20:33:39 -0300

    Atualização
    Atividade

---
+++ area_retangulo.ts
@@ -0,0 +1,7 @@
+function calcularAreaRetangulo(altura: number, largura: number){
+    const area = altura * largura
+    console.log(`A area é: ${area}`)
+    return area
+}
+
+calcularAreaRetangulo(5,3);
